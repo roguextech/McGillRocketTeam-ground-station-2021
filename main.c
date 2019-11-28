a64ef7f81b69a5b2923a674a9d8a02be7e92872a
Alex Stedman <alexander.stedman@mail.mcgill.ca>
2019-11-27 22:53:11 -0500

    Initial code setup to split and combine nibble

---
+++ main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+
+int combineNibbles(int leftNibble, int rightNibble);
+int* extractNibbles(char cIn);
+
+int main(int argc, const char* argv[]) {
+	int cIn = '';
+	int* temp;
+	temp = malloc((2 * sizeof(int)));
+	temp = extractNibbles(cIn);
+	int l = cIn >> 4;
+	int r = cIn % 16;
+	int cOut = combineNibbles(l, r);
+	printf("Character in: %c \t out: %c\n", cIn, cOut);
+	return 0;
+}
+
+int combineNibbles(int leftNibble, int rightNibble) {
+	printf("Left nibble: %d\nRight nibble: %d\n", leftNibble, rightNibble);
+	leftNibble = leftNibble << 4;
+	return leftNibble + rightNibble;
+}
+int* extractNibbles(char cIn) {
+	int *temp = malloc((2 * sizeof(int)));
+	*(temp) = cIn >> 4;
+	*(temp+1) = cIn % 16;
+	return temp;
+}
